6f858ca53b66fc3933694e594d9999e244c55ad0
Bernice998899 <155168978+Bernice998899@users.noreply.github.com>
2026-03-25 17:14:40 +0800

    Update phemex-history.js

--- api/phemex-history.js
+++ api/phemex-history.js
@@ -1,5 +1,4 @@
 const https = require('https');
-const crypto = require('crypto');
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,43 +8,17 @@ module.exports = (req, res) => {
   const symbol = req.query.symbol || 'BTCUSDT';
   const limit  = req.query.limit  || 50;
 
-  const apiKey    = process.env.PHEMEX_API_KEY;
-  const apiSecret = process.env.PHEMEX_API_SECRET;
+  const url = `https://api.phemex.com/exchange/public/md/funding-rate/history?symbol=${symbol}&limit=${limit}`;
 
-  const path = '/api-data/futures/funding-history';
-  const queryString = `symbol=${symbol}&limit=${limit}`;
-  const expiry      = Math.floor(Date.now() / 1000) + 60;
-
-  const message   = path + queryString + expiry;
-  const signature = crypto
-    .createHmac('sha256', apiSecret)
-    .update(message)
-    .digest('hex');
-
-  const options = {
-    hostname: 'api.phemex.com',
-    path: `${path}?${queryString}`,
-    method: 'GET',
-    headers: {
-      'x-phemex-access-token':      apiKey,
-      'x-phemex-request-expiry':    expiry,
-      'x-phemex-request-signature': signature,
-      'Content-Type': 'application/json',
-    }
-  };
-
-  const request = https.request(options, r => {
+  https.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } }, (r) => {
     let data = '';
     r.on('data', c => data += c);
     r.on('end', () => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).send(data);
     });
-  });
-
-  request.on('error', e => {
+  }).on('error', (e) => {
     res.status(502).json({ error: e.message });
   });
-
-  request.end();
 };
+```

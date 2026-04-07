b1ebdb0e50fe1e800d0b60ee9d0ba5fe268370eb
Bernice998899 <155168978+Bernice998899@users.noreply.github.com>
2026-04-08 01:27:38 +0800

    Update polymarket.js

--- api/polymarket.js
+++ api/polymarket.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const url = `https://gamma-api.polymarket.com/markets?limit=1000&active=true&tag_slug=weather`;
+  const url = `https://gamma-api.polymarket.com/markets?limit=1000&active=true`;
   const r = await fetch(url);
   const data = await r.json();
   res.json(data);
